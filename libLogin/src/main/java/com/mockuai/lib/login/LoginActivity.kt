a0cb4cc7c0febcc8c0fd00217720fa8b1c9e4c8f
liuwenchao <liuwenchao@mokuai.com>
2020-10-20 15:51:57 +0800

    依赖外部

---
+++ libLogin/src/main/java/com/mockuai/lib/login/LoginActivity.kt
@@ -0,0 +1,13 @@
+package com.mockuai.lib.login
+
+import android.app.Activity
+
+/**
+ * @author by:  awen on 2020/10/20  3:43 PM
+ * email     :  liuwenchao@mockuai.com
+ * Describe  :
+ */
+class LoginActivity : Activity() {
+
+
+}
