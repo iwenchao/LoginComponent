a38785cef69bd015ff681aaad463bc2a2079a858
liuwenchao <liuwenchao@mokuai.com>
2020-10-29 19:26:01 +0800

    子模块注释

--- libLogin/src/main/java/com/mockuai/lib/login/LoginActivity.kt
+++ libLogin/src/main/java/com/mockuai/lib/login/LoginActivity.kt
@@ -10,6 +10,7 @@ import android.os.Bundle
  */
 class LoginActivity : Activity() {
 
+    //
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
